2e2c7f2774d39b3146e35af5b1d560c710138f02
itzsouravkumar <oyesourav05012006@gamil.com>
2024-09-22 23:15:56 +0530

    initial commit

---
+++ UNIT-1/sum_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    //DEFINING VARIABLE
+    int a,b,c;
+    //TAKING INPUT
+    printf("Enter the numbers:");
+    scanf("%d %d",&a,&b);
+    //PROCESS
+    c=a+b;
+    //OUTPUT
+    printf("Sum:%d",c);
+    return 0;
+}
